3fad4a73759d832713e39549febabf2e1255d11f
Mahdi Asali <Mahdiasaly@gmail.com>
2021-02-06 11:02:05 +0200

    Adding new Page Details

--- DeerManager/App_Start/BundleConfig.cs
+++ DeerManager/App_Start/BundleConfig.cs
@@ -19,15 +19,6 @@ namespace DeerManager
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-
         }
     }
 }
